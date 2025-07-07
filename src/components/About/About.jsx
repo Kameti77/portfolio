5a116cbdeacaadd17a3c578e2fe747baa52c4671
Kameti77 <kametidanielk@gmail.com>
2025-07-06 21:50:27 -0400

    Initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
+
+export const About = () => {
+    return <section className={styles.container} id='about'>
+        <h2  className={styles.title}>About</h2>
+        <div  className={styles.content}><img
+            src={getImageUrl("about/aboutImage.png")}
+            alt="Me sitting with a laptop" 
+             className={styles.aboutImage}/>
+            <ul  className={styles.aboutItems}>
+                <li  className={styles.aboutItem}>
+                    <img src={getImageUrl("about/bulb.png")} alt="Bulb icon" />
+                    <div className={styles.aboutItemText}>
+                        <h2>What drives me:</h2>
+                        <p>A passion for building intuitive user interfaces and solving real-world problems with code.</p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+                    <div  className={styles.aboutItemText}>
+                        <h2>How I work:</h2>
+                        <p> I value clean code, responsive design, and a user-first mindset.</p>
+                    </div >
+                </li>
+                <li className={styles.aboutItem}>
+                    <img src={getImageUrl("about/cursorIcon.png")} alt="UI Icon" />
+                    <div  className={styles.aboutItemText}>
+                        <h2>Goals:</h2>
+                        <p>Grow as a developer through meaningful projects, internships, and collaboration.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>;
+};
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Contact.module.css";
+
+export const Contact = () =>{
+    return(<footer id="contact" className={styles.container}>
+        <div  className={styles.text}>
+            <h2>Get in Touch</h2>
+            <p>If you’re seeking a curious and adaptable learner passionate about coding and problem solving, let’s connect.</p>
+        </div>
+        <ul  className={styles.links}>
+            <li  className={styles.link}>
+                <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+                <a href="mailto:kametidanielk@gmail.com">kametidanielk@gmail.com</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon" />
+                <a href="https://www.linkedin.com/in/kameti-kumbi/">Kameti Kumbi</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageUrl("contact/githubIcon.png")} alt="GitHub icon" />
+                <a href="https://github.com/Kameti77">github.com/Kameti77</a>
+            </li>
+        </ul>
+    </footer>);
+};
